9ee709fbb93f1c028a19d555486718e48bfa375d
Raido <oras.raido@gmail.com>
2024-12-03 02:10:25 +0200

    day2: task one completed

---
+++ days/two/day_test.go
@@ -0,0 +1,13 @@
+package two
+
+import (
+	"advent/utils/errors"
+	"advent/utils/files"
+	"testing"
+)
+
+func TestDayTwoTaskOne(t *testing.T) {
+	fileLines, err := files.ReadFileLines("input")
+	errors.Check(err)
+	taskOne(fileLines)
+}
